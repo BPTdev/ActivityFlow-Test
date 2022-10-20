cd594ecd8402cf750714b2cf583f0ec0890de54c
Benoit <benoit.pierrehumbert.pro@gmail.com>
2022-10-20 11:02:28 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import Xarrow from "./xarrow";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+    <Xarrow />
+  </StrictMode>
+);
